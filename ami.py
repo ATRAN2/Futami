8c7a104451070aabf80e2ac5f570fbd34c8195bc
Timmy Zhu <nattofriends@gmail.com>
2015-04-05 21:47:58 -0700

    Add most functionality to API client side

---
+++ ami.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+from itertools import chain
+from operator import itemgetter
+from multiprocessing import (
+    Queue,
+    Process,
+)
+from time import sleep
+import json
+
+import requests
+
+from common import (
+    Action,
+    BoardTarget,
+    SubscriptionUpdate,
+    Post,
+    ThreadTarget,
+)
+
+SLEEP_TIME = 3  # seconds
+
+THREAD_LIST = "https://a.4cdn.org/{board}/threads.json"
+THREAD = "https://a.4cdn.org/{board}/res/{thread}.json"
+
+def flatten(lst):
+    return chain.from_iterable(lst)
+
+class Ami:
+    def __init__(self, response_queue):
+        self.response_queue = response_queue
+        self.update_request_queue = Queue()
+
+        self.update_worker = Process(target=self.poll_for_updates)
+        self.update_worker.start()
+
+    def request(self, request, identifier=None):
+        # The identifier argument is an opaque
+        # identifier used by the queue client in some situations.
+
+        if request.action is Action.LoadAndFollow:
+            if isinstance(request.target, BoardTarget):
+                # Download all threads
+                board = request.target.board
+                threads = self.get_board(board)
+
+                # Seed seen_boards so the update loop doesn't re-fetch all of
+                # them
+                self.update_request_queue.put(SubscriptionUpdate(
+                    Action.InternalQueueUpdate,
+                    (board, {thread['no']: thread['last_modified'] for thread in threads})
+                ))
+
+                threads.sort(key=itemgetter('last_modified'))
+
+                # Download all thread content so we can get the OP
+                for thread in threads:
+                    posts = list(self.get_thread(board, thread['no']))
+                    op = posts[0]
+                    op.identifier = identifier
+
+                    self.response_queue.put(op)
+
+            elif isinstance(request.target, ThreadTarget):
+                posts = list(self.get_thread(request.target.board, request.target.thread))
+
+                for post in posts:
+                    post.identifier = identifier
+
+                    self.response_queue.put(post)
+
+        self.update_request_queue.put(request)
+
+    def get_board(self, board):
+        url = THREAD_LIST.format(board=board)
+        pages = requests.get(url).json()
+        threads = list(flatten([page['threads'] for page in pages]))
+        return threads
+
+    def get_thread(self, board, thread):
+        url = THREAD.format(board=board, thread=thread)
+        posts = requests.get(url).json()['posts']
+
+        for post in posts:
+            post['board'] = board
+
+        posts = map(Post, posts)
+
+        return posts
+
+    def poll_for_updates(self):
+        # List of boards that are watched
+        watched_boards = set()
+        # Dictionary of board => list of threads(string) that are watched
+        watched_threads = defaultdict(list)
+        seen_boards = defaultdict(list)
+
+        while True:
+            # Process pending update requests
+            while not self.update_request_queue.empty():
+                request = self.update_request_queue.get()
+                if request.action is Action.LoadAndFollow:
+                    if isinstance(request.target, BoardTarget):
+                        # The InternalQueueUpdate the is fired _before_ this
+                        # event will have added the board for us.
+                        pass
+                    elif isinstance(request.target, ThreadTarget):
+                        assert request.target.board in watched_boards, "Asked to watch a thread of a board not currently being watched"
+                        watched_threads[request.target.board].append(request.target.thread)
+                elif request.action is Action.InternalQueueUpdate:
+                    board, seen = request.target
+                    watched_boards.add(board)
+                    seen_boards[board] = seen
+
+            # Fetch pending boards
+            pending_boards = defaultdict(dict)
+            for board in watched_boards:
+                pending_boards[board] = {thread['no']: thread['last_modified'] for thread in self.get_board(board)}
+
+            to_delete = []
+            for board, threads in pending_boards.items():
+                for thread_no, last_modified in threads.items():
+                    if thread_no not in seen_boards[board]:
+                        thread = list(self.get_thread(board, thread_no))[0]
+                        self.response_queue.put(thread)
+                    elif last_modified > seen_boards[board][thread_no]:
+                        thread = list(self.get_thread(board, thread_no))[0]
+                        self.response_queue.put(thread)
+                    elif last_modified < seen_boards[board][thread_no]:
+                        # Sometimes we get stale data immediately after reading
+                        # it (tested under SLEEP_TIME = 3). Ignore this data.
+                        to_delete.append((board, thread_no))
+
+            for board, thread_no in to_delete:
+                del pending_boards[board][thread_no]
+
+            # Fetch pending threads
+            # TODO: Not implemented yet
+
+            seen_boards = pending_boards
+
+            sleep(SLEEP_TIME)
+
+if __name__ == "__main__":
+
+    request = SubscriptionUpdate(Action.LoadAndFollow, BoardTarget('a'))
+    # request2 = SubscriptionUpdate(Action.LoadAndFollow, ThreadTarget('jp', 13265004))
+    response_queue = Queue()
+
+    ami = Ami(response_queue)
+    ami.request(request)
+    # ami.request(request2)
+
+
+
---
+++ common.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+import enum
+
+SubscriptionUpdate = namedtuple('SubscriptionUpdate', ['action', 'target'])
+
+BoardTarget = namedtuple('BoardTarget', ['board'])
+
+ThreadTarget = namedtuple('ThreadTarget', ['board', 'thread'])
+
+class Action(enum.Enum):
+    LoadAndFollow = 1
+    Stop = 2
+
+    InternalQueueUpdate = 100
+
+
+class Image(namedtuple('Image', ['filename', 'tim', 'ext', 'fsize', 'md5', 'w', 'h', 'tn_w', 'tn_h', 'board'])):
+    IMAGE = "https://i.4cdn.org/{board}/src/{tim}{ext}"
+    IMAGE_THUMB = "https://t.4cdn.org/{board}/thumb/{tim}s.jpg"
+
+    @property
+    def image_url(self):
+        return
+
+    @property
+    def thumb_url(self):
+        return
+
+    @property
+    def filesize(self):
+        return self.fsize
+
+    def __repr__(self):
+        return "<Image {0}{1} ({2}x{3})>".format(self.filename, self.ext, self.w, self.h)
+
+
+class Post:
+    # Because moot is bad at updating the API documentation, the second set of fields are not
+    # listed in the API documentation.
+    # The third set of fields are values that are synthesized by us.
+    post_fields = [
+        'no', 'resto', 'sticky', 'closed', 'now', 'time', 'name', 'trip', \
+        'filename', 'id', 'capcode', 'country', 'country_name', 'email', 'sub', 'com', 'tim', \
+        'ext', 'fsize', 'md5', 'w', 'h', 'tn_w', 'tn_h', 'filedeleted', 'spoiler', \
+        'custom_spoiler', 'omitted_posts', 'omitted_images', 'replies', 'images', \
+        'bumplimit', 'imagelimit', 'capcode_replies', 'last_modified', 'tag', 'semantic_url',
+
+        'unique_ips',
+
+        'board',
+    ]
+
+    interface_field_map = {
+        'post_no': 'no',
+        'reply_to': 'resto',
+        'poster_name': 'name',
+        'tripcode': 'trip',
+        'id': 'id',
+        'subject': 'sub',
+        'comment': 'com',
+        'board': 'board',
+    }
+
+    def __init__(self, data):
+        missing_fields = set(self.post_fields).difference(data.keys())
+        data.update({field: None for field in missing_fields})
+
+        self.data = data
+
+        self._image = Image(
+            self.data['filename'],
+            self.data['tim'],
+            self.data['ext'],
+            self.data['fsize'],
+            self.data['md5'],
+            self.data['w'],
+            self.data['h'],
+            self.data['tn_w'],
+            self.data['tn_h'],
+            self.data['board'],
+        )
+
+    def __getattr__(self, name):
+        if name in self.interface_field_map:
+            return self.data[self.interface_field_map[name]]
+        raise AttributeError("'{0}' object has no attribute '{1}'".format(
+            self.__class__.__name__,
+            name,
+        ))
+
+    @property
+    def image(self):
+        return self._image
+
+    def __repr__(self):
+        return "<Post {0}/{1}>".format(self.board, self.post_no)
+
+    def __eq__(self, other):
+        return isinstance(other, Post) and self.post_no == other.post_no
